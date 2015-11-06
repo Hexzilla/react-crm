010a7c481a59855479c92138bd515984ebfd53fe
Hexzilla <hexzilla256@gmail.com>
2015-11-06 10:12:29 +0300

    app nav and footer

---
+++ client/app/app-body.js
@@ -0,0 +1,18 @@
+//import './app-body.html';
+
+//import { FlowRouter } from 'kadira:flow-router';
+
+if (Meteor.isClient) {
+    Template.AppBody.onCreated(function () {
+        console.log("AppBody.onCreated");
+    });
+}
+
+
+
+Template.AppBody.events({
+    'click button': function () {
+        // increment the counter when button is clicked
+        console.log("button .sidebarToggle");
+    }
+});
