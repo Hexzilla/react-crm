f5fca3aaa2b189a9db136a4f595292e91c38fd07
Hexzilla <hexzilla256@gmail.com>
2015-11-06 10:11:28 +0300

    rid app-body

--- client/app-body.js
+++
@@ -1,9 +0,0 @@
-//import './app-body.html';
-
-//import { FlowRouter } from 'kadira:flow-router';
-
-if (Meteor.isClient) {
-    Template.AppBody.onCreated(function () {
-        console.log("AppBody.onCreated");
-    });
-}
