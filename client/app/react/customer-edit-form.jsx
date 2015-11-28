416ef0d27ca412bcf889fa3be6a89cae46eca470
Hexzilla <hexzilla256@gmail.com>
2015-11-28 10:26:31 +0300

    react components

---
+++ client/app/react/customer-edit-form.jsx
@@ -0,0 +1,68 @@
+
+//var Input = require('./textInput');
+
+// App component - represents the whole app
+CustomerEditForm = React.createClass({
+    // Declare our expectations for using this class (not mandatory, just good practice)
+    propTypes: {
+        customer: React.PropTypes.object.isRequired,
+        onChange: React.PropTypes.func.isRequired,
+        onSave: React.PropTypes.func.isRequired,
+        errors: React.PropTypes.object // this one is optional (no isRequired)
+    },
+
+    render() {
+        console.log("CustomerEditComponent customer: ", this.props.customer);
+        return (
+            <form className="customer_edit_react" onSubmit={this.props.onSave}>
+                <div className="panel panel-default col-md-6">
+                    <div className="panel-body">
+
+                        <h3>{this.props.customer.name}</h3>
+
+                        <Input
+                            name="name"
+                            label="Name"
+                            onChange = {this.props.onChange}
+                            placeholder="Name"
+                            value={this.props.customer.name}
+                            error={this.props.errors.name}
+                        />
+
+                        <Input
+                            name="email"
+                            label="Email"
+                            onChange = {this.props.onChange}
+                            placeholder="Email"
+                            value={this.props.customer.email}
+                            error={this.props.errors.email}
+                        />
+
+                        <Input
+                            name="postcode"
+                            label="Postcode"
+                            onChange = {this.props.onChange}
+                            placeholder="Postcode"
+                            value={this.props.customer.postcode}
+                            error={this.props.errors.postcode}
+                        />
+
+
+                        <input
+                            type="submit"
+                            value="Save"
+                            className="btn btn-primary"
+                            onClick={this.props.onSave}
+                        />
+
+                    </div>
+                </div>
+            </form>
+        );
+    }
+});
+//<div className="form-group">
+//    <label for="nextContactDate">Next contact date</label>
+//    <input type="text" id="nextContactDate" className="form-control"
+//           defaultValue={this.props.customer.nextContactDate}/>
+//</div>
---
+++ client/app/react/customer-edit-page.jsx
@@ -0,0 +1,80 @@
+
+//var Input = require('./textInput');
+
+// App component - represents the whole app
+CustomerEditPage = React.createClass({
+    propTypes: {
+        customer: React.PropTypes.object.isRequired,
+        onSave: React.PropTypes.func.isRequired
+    },
+
+    getInitialState() {
+        console.log("CustomerEditPage.getInitialState");
+        return {
+            errorsList: new ReactiveDict(),
+            customer: {},
+            errors: {}
+        };
+    },
+
+    componentDidMount() {
+        console.log("CustomerEditPage.componentDidMount");
+        this.setState({customer: this.props.customer});
+    },
+
+
+    onChangeHandler: function (event) {
+        console.log("onChangeHandler event ", event);
+        console.log("this.customer before ", this.state.customer);
+
+        // update our customer data to reflect the new value in the UI
+        var field = event.target.name;
+        var value = event.target.value;
+        this.state.customer[field] = value;
+
+        console.log("this.customer after", this.state.customer);
+
+        var schemaContext = Schemas.CustomerCompaniesSchema.namedContext("customerEditReactForm");
+
+        this.state.errors = {};
+
+        schemaContext.validate(this.state.customer);
+        console.log( "schemaContext.invalidKeys", schemaContext.invalidKeys());
+
+        schemaContext.invalidKeys().forEach(invalidKey => {
+            console.log("invalidKey: ", invalidKey);
+            var errMessage = schemaContext.keyErrorMessage(invalidKey.name);
+            console.log("keyErrorMessage: ", errMessage);
+
+            if (invalidKey.name!="_id") {
+                this.state.errors[invalidKey.name] = errMessage;
+            } else {
+            }
+
+
+        });
+        
+        console.log("this.state.errors", this.state.errors);
+
+        return this.setState({customer: this.state.customer});
+    },
+
+    render() {
+        console.log("render state ", this.state);
+
+        return (
+            <CustomerEditForm
+                customer={this.state.customer}
+                onChange={this.onChangeHandler}
+                onSave={this.props.onSave}
+                errors={this.state.errors}
+            />
+        );
+    }
+});
+//<div className="form-group">
+//    <label for="nextContactDate">Next contact date</label>
+//    <input type="text" id="nextContactDate" className="form-control"
+//           defaultValue={this.props.customer.nextContactDate}/>
+//</div>
+
