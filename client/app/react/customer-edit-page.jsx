fd256760083214fad82d21aa1c8afc113b3dd537
Hexzilla <hexzilla256@gmail.com>
2015-11-30 09:47:55 +0300

    customer edit page

--- client/app/react/customer-edit-page.jsx
+++ client/app/react/customer-edit-page.jsx
@@ -1,7 +1,7 @@
 
-//var Input = require('./textInput');
+//var TextInput = require('./textInput');
 
-// App component - represents the whole app
+// this page is wrapped by the wrapper
 CustomerEditPage = React.createClass({
     propTypes: {
         customer: React.PropTypes.object.isRequired,
@@ -9,72 +9,63 @@ CustomerEditPage = React.createClass({
     },
 
     getInitialState() {
-        console.log("CustomerEditPage.getInitialState");
+        //console.log("CustomerEditPage.getInitialState");
         return {
             errorsList: new ReactiveDict(),
-            customer: {},
-            errors: {}
+            customer: this.props.customer,
+            errors: {},
+            isValid: false
         };
     },
 
-    componentDidMount() {
-        console.log("CustomerEditPage.componentDidMount");
-        this.setState({customer: this.props.customer});
-    },
-
-
     onChangeHandler: function (event) {
-        console.log("onChangeHandler event ", event);
-        console.log("this.customer before ", this.state.customer);
 
-        // update our customer data to reflect the new value in the UI
+        // update our customer state to reflect the new value in the UI
         var field = event.target.name;
         var value = event.target.value;
         this.state.customer[field] = value;
 
-        console.log("this.customer after", this.state.customer);
-
-        var schemaContext = Schemas.CustomerCompaniesSchema.namedContext("customerEditReactForm");
-
         this.state.errors = {};
 
+        // validate the customer against the table schema
+        var schemaContext = Schemas.CustomerCompaniesSchema.namedContext("customerEditReactForm");
         schemaContext.validate(this.state.customer);
-        console.log( "schemaContext.invalidKeys", schemaContext.invalidKeys());
 
         schemaContext.invalidKeys().forEach(invalidKey => {
-            console.log("invalidKey: ", invalidKey);
             var errMessage = schemaContext.keyErrorMessage(invalidKey.name);
-            console.log("keyErrorMessage: ", errMessage);
-
-            if (invalidKey.name!="_id") {
+            if (invalidKey.name != "_id") {
                 this.state.errors[invalidKey.name] = errMessage;
-            } else {
             }
-
-
         });
-        
-        console.log("this.state.errors", this.state.errors);
 
+        this.setFormIsValid();
+
+        // Update the state, this will then cause the re-render
         return this.setState({customer: this.state.customer});
     },
 
-    render() {
-        console.log("render state ", this.state);
+    setFormIsValid: function() {
+        this.state.isValid = (Object.keys(this.state.errors).length === 0);
+    },
 
+    saveCustomer(event) {
+        event.preventDefault();
+
+        this.props.onSave(this.state.customer);
+    },
+
+    render() {
+        this.setFormIsValid();
+
+        //console.log("render state ", this.state);
         return (
             <CustomerEditForm
                 customer={this.state.customer}
                 onChange={this.onChangeHandler}
-                onSave={this.props.onSave}
+                onSave={this.saveCustomer}
                 errors={this.state.errors}
+                isValid={this.state.isValid}
             />
         );
     }
 });
-//<div className="form-group">
-//    <label for="nextContactDate">Next contact date</label>
-//    <input type="text" id="nextContactDate" className="form-control"
-//           defaultValue={this.props.customer.nextContactDate}/>
-//</div>
-
