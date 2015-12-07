35dfb12e15e69b805e38e4addc799fa18c25ff74
Hexzilla <hexzilla256@gmail.com>
2015-12-07 11:24:04 +0300

    customers list

--- client/customers/customers-list.jsx
+++ client/customers/customers-list.jsx
@@ -1,6 +1,7 @@
 // App component - represents the whole app
 
 import React from 'react';
+import { Link } from 'react-router';
 
 CustomersList = React.createClass({
     // This mixin makes the getMeteorData method work
@@ -21,35 +22,60 @@ CustomersList = React.createClass({
         return data;
     },
 
-    renderCustomerListItem() {
-        console.log("customers2", this.data.customers)
+
+
+    renderCustomerTable() {
+        //console.log("customers2", this.data.customers)
+
+        // Get tasks from this.data.tasks
+        return (
+            <table className="table table-responsive table-striped">
+                <tbody>
+                <tr>
+                    <th>Customer name</th>
+                    <th>Postcode</th>
+                    <th></th>
+                </tr>
+                {this.renderCustomerListItems()}
+                </tbody>
+            </table>
+
+        )
+    },
+
+    renderCustomerListItems() {
+        //console.log("customers2", this.data.customers)
 
         // Get tasks from this.data.tasks
         return this.data.customers.map((customer) => {
 
-            return <CustomerListItem key={customer._id} customer={customer}/>;
+            return (
+
+                <tr key={customer._id}>
+                    <td>{customer.name}</td>
+                    <td>{customer.postcode}</td>
+                    <td><Link className="btn btn-default btn-sm" to={`/customers/${customer._id}`}>Edit</Link></td>
+                </tr>
+
+            );
         });
     },
 
+
+
     render() {
         return (
-                <div className="panel panel-default">
-                    <div className="panel-heading"><a href="#" className="pull-right">View all</a>
-                        <h4>Top customers</h4>
-                    </div>
-                    <div className="panel-body">
-                        <div className="col-md-6">
-
-
-                            <ul>
-                                { this.data.customers ?
-                                    this.renderCustomerListItem() :
-                                    null }
-                            </ul>
-                        </div>
-                    </div>
+            <div className="panel panel-default">
+                <div className="panel-heading"><a href="#" className="pull-right">View all</a>
+                    <h4>Top Customers</h4>
                 </div>
+                <div className="panel-body">
 
+                        { this.data.customers ?
+                                this.renderCustomerTable() :
+                                <p>Loading</p> }
+                </div>
+            </div>
         );
     }
 });
