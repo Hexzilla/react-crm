03a35a8e88bca1e1515f712e14c6d7bc7b38a7f3
Hexzilla <hexzilla256@gmail.com>
2015-12-18 10:05:31 +0300

    order methods api

---
+++ api/orders/methods.js
@@ -0,0 +1,43 @@
+
+import Orders from './order.js';
+
+Orders.methods = {};
+
+
+// Manual form specific update method that knows how to unpack the single
+// object we get from autoform.
+Orders.methods.upsert = new ValidatedMethod({
+
+    // register the name
+    name: 'Orders.methods.upsert',
+
+    validate(args) {
+        console.log("validating order", args);
+
+        var schemaContext = Schemas.OrderSchema.namedContext("OrderForm");
+        schemaContext.validate(args.data);
+
+        console.log("validation succeeded");
+    },
+
+
+    // the actual database updating part
+    // validate has already been run at this point
+    run(args) {
+        console.log("run");
+        console.log("args", args);
+        console.log("lines", args.data.orderLines);
+        console.log("lines[0]", args.data.orderLines[0]);
+        console.log("order", JSON.stringify(args.data));
+
+        return Orders.upsert(args.orderId, {$set: args.data});
+    }
+});
+
+
+Meteor.methods({
+    'Orders.methods.remove'({ orderId }) {
+        console.log("Orders.methods.remove", orderId);
+        Orders.remove( { _id: orderId } );
+    }
+});
---
+++ api/orders/server/publications.js
@@ -0,0 +1,13 @@
+
+//import { Orders } from '../customer-company.js';
+
+Meteor.publish('Orders.public', function() {
+    return Orders.find();
+});
+
+Meteor.publish('Order.get', function (id) {
+
+    //console.log("publication match ", Orders.find({_id: id}).fetch());
+
+    return Orders.find({_id: id});
+});
