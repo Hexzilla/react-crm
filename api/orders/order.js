466d71edc1fa456af19d426ae50d6c26802e0688
Hexzilla <hexzilla256@gmail.com>
2015-12-21 09:04:55 +0300

    update server order api

--- api/orders/order.js
+++ api/orders/order.js
@@ -1,10 +1,12 @@
+import { recalculateOrderTotals } from '../../lib/order-logic';
+
 class ordersCollection extends Mongo.Collection {}
 
 // Make it available to the rest of the app
-Orders = new ordersCollection("Orders");
+const Orders = new ordersCollection("Orders");
 
 // Deny all client-side updates since we will be using methods to manage this collection
-Orders.allow({
+Orders.deny({
     insert() { return true; },
     update() { return true; },
     remove() { return true; }
@@ -19,3 +21,21 @@ Orders.allow({
 // Collection2 is what's allowing this to happen
 Orders.attachSchema(Schemas.OrderSchema);
 
+Orders.before.insert(function (userId, doc) {
+    console.log("Orders.before.insert", doc);
+    // Ensure all the line totals acrea correctly set, even if the UI already did this.
+    recalculateOrderTotals(doc);
+});
+
+Orders.before.update(function (userId, doc, fieldNames, modifier, options) {
+    console.log("Orders.before.update", doc);
+    // Ensure all the line totals acrea correctly set, even if the UI already did this.
+    recalculateOrderTotals(doc);
+});
+
+Orders.before.upsert(function (userId, selector, modifier, options) {
+    // Ensure all the line totals acrea correctly set, even if the UI already did this.
+    recalculateOrderTotals(modifier.$set);
+});
+
+export default Orders;
