21362f143e067288b980117b2d1e7ff44a0114fb
Hexzilla <hexzilla256@gmail.com>
2016-01-10 08:51:06 +0300

    update server publications

--- api/customers/server/publications.js
+++ api/customers/server/publications.js
@@ -1,18 +1,20 @@
 
 //import { CustomerCompanies } from '../customer-company.js';
 
-Meteor.publish('CustomerCompanies.public', function() {
+Meteor.publish('CustomerCompanies.public', function () {
     return CustomerCompanies.find();
 });
 
-Meteor.publish('CustomerCompany.get', function (custId) {
-
+Meteor.publish('CustomerCompany.get', function (_id) {
     //console.log("publication match ", CustomerCompanies.find({_id: custId}).fetch());
 
-    return CustomerCompanies.find({_id: custId});
+    return CustomerCompanies.find({_id});
 });
 
-Meteor.publish('SalesRegions.All', function() {
-    return SalesRegions.find();
-});
+Meteor.publish('CustomerCompanies.searchByName', function (searchTerm) {
+    // console.log("CustomerCompanies.searchByName - "
+    //     + searchTerm + " - ", CustomerCompanies.find({name: new RegExp(searchTerm)}).fetch());
 
+    // the 'i' makes the search case insensitive
+    return CustomerCompanies.find({name: new RegExp(searchTerm, 'i')});
+})
