e15b0d6e4e2fdd863c2dce66757a80eed0bae62b
Hexzilla <hexzilla256@gmail.com>
2016-01-27 16:12:43 +0300

    sales order line edit

--- client/sales/order-line-edit.jsx
+++ client/sales/order-line-edit.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import AsyncSelectInput from '../controls/asyncSelectInput.jsx';
+import Products from '../../api/products/products';
+import accounting from 'accounting';
 
 const OrderLineEdit = React.createClass({
     propTypes: {
         orderLine: React.PropTypes.object,
         onChange: React.PropTypes.func.isRequired,
+        onProductChange: React.PropTypes.func.isRequired,
         deleteOrderLine: React.PropTypes.func,
         errors: React.PropTypes.object.isRequired
     },
 
-
     getInitialState() {
-
-        console.log("OrderPage.getInitialState", this.props);
+        //console.log("OrderLineEdit.getInitialState", this.props);
 
         return {
             errors: {},
@@ -19,69 +21,99 @@ const OrderLineEdit = React.createClass({
         };
     },
 
-
     handleChange(event) {
-        var field = event.target.name;
-        var value = event.target.value;
+        const field = event.target.name;
+        const value = event.target.value;
 
         this.props.onChange(this.props.orderLine._id, field, value);
+    },
+
+    handleProductChange(selectedItem) {
+        this.props.onProductChange(this.props.orderLine._id, selectedItem);
     },
 
     deleteLine() {
         this.props.deleteOrderLine(this.props.orderLine._id);
     },
 
+    getProducts: function getProducts(input) {
+        console.log("OrderHeaderEdit.getProducts()", input);
+        const handle = Meteor.subscribe('Products.searchByName', input);
+        return Products.find().fetch();
+    },
 
     render() {
         //console.log("OrderLineEdit props: ", this.props);
-        let deleteButton;
+
+        const value = {
+            _id: this.props.orderLine.productId ? this.props.orderLine.productId : null,
+            name: this.props.orderLine.description
+        };
 
         // only show the delete button if we are passed in a delete method
+        let deleteButton;
         if (this.props.deleteOrderLine) {
             deleteButton =
-                <div className="form-group">
-                    <input
-                        type="button"
-                        className="btn btn-warning"
-                        id="deleteOrderLineButton"
-                        onClick={this.deleteLine}
-                        value="Delete line"
-                    />
-                </div>;
+                <input
+                    type="button"
+                    className="btn btn-warning"
+                    id="deleteOrderLineButton"
+                    onClick={this.deleteLine}
+                    value="Delete line"
+                />
         }
 
         return (
-            <div >
 
-                <TextInput
-                    name="description"
-                    onChange={this.handleChange}
-                    value={this.props.orderLine.description}
-                    error={this.props.errors.description}
-                />
+            <tr key={this.props.orderLine._id}>
 
-                <NumberInput
-                    name="quantity"
-                    onChange={this.handleChange}
-                    value={this.props.orderLine.quantity}
-                    error={this.props.errors.quantity}
-                />
+                <td data-th="Product" className="row col-sm-10 numeric">
+                    <AsyncSelectInput
+                        name="productId"
+                        label="Product"
+                        value={value}
+                        onChange={this.handleProductChange}
+                        error={this.props.errors.productId}
+                        loadOptions={this.getProducts}
+                        valueKey="_id"
+                        labelKey="name"
+                        hideLabel={true}
+                    />
 
+                </td>
+                <td data-th="Quantity" className="numeric">
+                    <NumberInput
+                        name="quantity"
+                        onChange={this.handleChange}
+                        value={this.props.orderLine.quantity}
+                        error={this.props.errors.quantity}
+                        hideLabel={true}
+                    />
+                </td>
 
-                <NumberInput
-                    name="unitPrice"
-                    onChange={this.handleChange}
-                    value={this.props.orderLine.unitPrice}
-                    error={this.props.errors.unitPrice}
-                />
+                <td data-th="Unit Price" className="numeric">
+                    <NumberInput
+                        name="unitPrice"
+                        onChange={this.handleChange}
+                        value={this.props.orderLine.unitPrice}
+                        error={this.props.errors.unitPrice}
+                        hideLabel={true}
+                        isMoney={true}
+                        decimalPlaces={2}
+                    />
+                </td>
 
-                <div className="form-group">
-                    <label>Line value:</label>
-                    <label>{this.props.orderLine.unitPrice * this.props.orderLine.quantity}</label>
-                </div>
+                <td data-th="Sub Total" className="form-group numeric">
+                    <p className="sub-total numeric">
+                        {accounting.formatMoney(this.props.orderLine.unitPrice * this.props.orderLine.quantity, '£')}
+                    </p>
+                </td>
 
-                {deleteButton}
-            </div>
+                <td className="actions" data-th="">
+                    {deleteButton}
+                </td>
+            </tr>
+
         );
     }
 });
