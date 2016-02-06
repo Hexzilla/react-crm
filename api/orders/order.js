1d9b94beae73d1ba4d59d447e75066f25f0dafea
Hexzilla <hexzilla256@gmail.com>
2016-02-06 08:38:21 +0300

    update topbar and orders

--- api/orders/order.js
+++ api/orders/order.js
@@ -13,10 +13,6 @@ Orders.deny({
     remove() { return true; }
 });
 
-// Define the expected Schema for data going into,
-// and coming out of the database
-//Orders.schema = Schemas.OrdersSchema
-
 // Bolt that schema onto the collection so that all mutator
 // calls are automatically checked against the schema.
 // Collection2 is what's allowing this to happen
@@ -50,21 +46,33 @@ Orders.after.update(function (userId, doc, fieldNames, modifier, options) {
 
 
 const customerCompanyDenormalizer = {
+
+    // Ensure that no matter what customerName we receive from the client,
+    // the correct name for the selected customerId is always set.
     _updateCompanyNameOnOrder(order) {
-        //console.log("customerCompanyDenormalizer._updateCompanyNameOnOrder() ",
-        //    order.customerId + " - " + order.customerName);
 
-        // no action needed if the customerId is not set
-        if (!order.customerId || order.customerId === null) { return; }
+        // We only want to do this update on the server - it was already done on the client
+        // And wouldn't work on the client as we are accessing the customer table directly
+        // and also because the client miniMongo data subset may not contain the customer
+        // at that point in time
+        if (Meteor.isServer) {
+            console.log("customerCompanyDenormalizer._updateCompanyNameOnOrder() ",
+                order.customerId + " - " + order.customerName);
 
-        //const handle = Meteor.subscribe('CustomerCompany.get', order.customerId);
-        //const customer = CustomerCompanies.findOne({_id: order.customerId});
-        //
-        //if (!customer) {
-        //    throw new Meteor.Error("The customer could not be found in the database");
-        //}
-        //
-        //order.customerName = customer.name;
+            // no action needed if the customerId is not set
+            if (!order.customerId || order.customerId === null) {
+                return;
+            }
+
+            //const handle = Meteor.subscribe('CustomerCompany.get', order.customerId);
+            const customer = CustomerCompanies.findOne({_id: order.customerId});
+
+            if (!customer) {
+                throw new Meteor.Error("The customer could not be found in the database");
+            }
+
+            order.customerName = customer.name;
+        }
     },
 
     _updateCompanyOrderTotals(customerId, previousCustomerId) {
@@ -76,12 +84,11 @@ const customerCompanyDenormalizer = {
                 return;
             }
 
-            let customerIds = [customerId];
+            let customerIds = [ customerId ];
 
             // if the customer Id changed we also need to update the order totals for
             // the old customer
-            if (customerId !== previousCustomerId)
-            {
+            if (customerId !== previousCustomerId) {
                 customerIds.push(previousCustomerId);
             }
 
@@ -110,12 +117,12 @@ const customerCompanyDenormalizer = {
                 CustomerCompanies.update(thisCustomerId, {
                     $set: {
                         // the result will be null if this customer now has no orders
-                        ordersTotalValue:  result ? result.ordersTotalValue : 0,
-                        ordersCount:result ? result.ordersCount : 0
+                        ordersTotalValue: result ? result.ordersTotalValue : 0,
+                        ordersCount: result ? result.ordersCount : 0
                         //email: "hi@hi.com" //+ new Date().toTimeString()
                     }
                 });
-            })
+            });
 
             //console.log("_updateCompanyOrderTotals Completed");
         }
@@ -132,24 +139,24 @@ const customerCompanyDenormalizer = {
     },
     
     beforeInsert(userId, doc) {
-        this._performCommonBeforeModifyActions(doc)
+        this._performCommonBeforeModifyActions(doc);
     },
 
     beforeUpdate(userId, doc, fieldNames, modifier, options) {
-        this._performCommonBeforeModifyActions(doc)
+        this._performCommonBeforeModifyActions(doc);
     },
 
     beforeUpsert(userId, selector, modifier, options) {
-        this._performCommonBeforeModifyActions(modifier.$set)
+        this._performCommonBeforeModifyActions(modifier.$set);
     },
 
     afterInsert(userId, doc) {
-        this._performCommonAfterModifyActions(doc)
+        this._performCommonAfterModifyActions(doc);
     },
 
     afterUpdate(userId, doc, fieldNames, modifier, options, previousDoc) {
         console.log("previousDoc: ", previousDoc);
-        this._performCommonAfterModifyActions(doc, previousDoc)
+        this._performCommonAfterModifyActions(doc, previousDoc);
     }   
 
 };
