099c4679e1155142618ec16d200c0b09b452d6fa
Hexzilla <hexzilla256@gmail.com>
2016-02-08 09:04:07 +0300

    company full search

--- api/customers/server/publications.js
+++ api/customers/server/publications.js
@@ -57,3 +57,31 @@ Meteor.publish('CustomerCompanies.fullTextSearch', function (searchValue) {
     );
 });
 
+Meteor.methods({
+    'CustomerCompanies.fullTextSearch.method'({ searchValue }) {
+
+        if (Meteor.isServer) {
+            const results = CustomerCompanies.find(
+                {$text: {$search: searchValue}},
+                {
+                    // `fields` is where we can add MongoDB projections. Here we're causing
+                    // each document published to include a property named `score`, which
+                    // contains the document's search rank, a numerical value, with more
+                    // relevant documents having a higher score.
+                    fields: {
+                        score: {$meta: "textScore"},
+                        name: 1 // Only return the name column (_id is returned automatically)
+                    },
+                    // This indicates that we wish the publication to be sorted by the
+                    // `score` property specified in the projection fields above.
+                    sort: {
+                        score: {$meta: "textScore"}
+                    }
+                }
+            );
+            //console.log('CustomerCompanies.fullTextSearch results ', results.fetch());
+
+            return results.fetch();
+        }
+    }
+});
