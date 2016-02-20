4e555925a33fb7038b9fd239f757fcbd4ec51a80
Hexzilla <hexzilla256@gmail.com>
2016-02-20 09:28:52 +0300

    update app.jsx

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -5,20 +5,60 @@
 
 
 import React from 'react';
-import MessageEditor from '../controls/modal-message-box.jsx';
-
-//var RouteHandler = require('react-router').RouteHandler;
-//var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 
 Meteor.subscribe("SalesRegions.All");
 Meteor.subscribe("Orders.All");
 //Meteor.subscribe("Products.public");
 
+//var checkScrollBars = function(){
+//    var b = $('body');
+//    var normalw = 0;
+//    var scrollw = 0;
+//    if(b.prop('scrollHeight')>b.height()){
+//        normalw = window.innerWidth;
+//        scrollw = normalw - b.width();
+//        $('#container').css({marginRight:'-'+scrollw+'px'});
+//    }
+//}
+//
+//checkScrollBars();
+
+const ContentContainer = React.createClass({
+
+    componentWillUnmount() {
+        console.log("ContentContainer.componentWillUnmount");
+    },
+
+    render() {
+        console.log("ContentContainer.render()", this.props.children);
+
+
+        return (
+            <div key="contentContainer">
+                <ReactCSSTransitionGroup
+                    component="div"
+                    transitionName="example"
+                    transitionEnterTimeout={300}
+                    transitionLeaveTimeout={300}
+                >
+                    {React.cloneElement(this.props.children, {
+                        key: this.props.children.key
+                    })}
+                </ReactCSSTransitionGroup>
+            </div>
+        );
+    }
+
+});
+
+
 // define and export our Layout component
 export const Layout = ({content}) => (
-
     <div id="app">
+        {console.log("Layout rendered")}
+
         <div className="wrapper">
             <div className="box">
                 <div className="row row-offcanvas row-offcanvas-left">
@@ -37,8 +77,11 @@ export const Layout = ({content}) => (
                                     <main>
                                         <div id="popup"></div>
 
-                                        <div>{content}</div>
+                                        <div id="outerContent">
 
+                                            <ContentContainer key="content">{content}</ContentContainer>
+
+                                        </div>
                                     </main>
                                 </div>
                             </div>
