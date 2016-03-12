e447fe89cbfcd5bc16601e3fb5e6b645bcdc6f9b
Hexzilla <hexzilla256@gmail.com>
2016-03-12 10:09:52 +0300

    update routes

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -9,10 +9,24 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import { Provider } from 'react-redux';
 import DevTools from '../redux/DevTools.jsx';
 import store from '../redux/store.jsx';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import TopNavContainer from './top-nav.jsx';
+import SidebarContainer from './sidebar.jsx';
+import MainContentContainer from './MainContentContainer.jsx';
+
+import {Spacing} from 'material-ui/lib/styles';
+import {StyleResizable} from 'material-ui/lib/mixins';
+
+import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
+import {darkWhite, lightWhite, grey900} from 'material-ui/lib/styles/colors';
+
+
+
 
 Meteor.subscribe("SalesRegions.All");
 Meteor.subscribe("Orders.All");
-//Meteor.subscribe("Products.public");
+
 
 //var checkScrollBars = function(){
 //    var b = $('body');
@@ -27,7 +41,27 @@ Meteor.subscribe("Orders.All");
 //
 //checkScrollBars();
 
+injectTapEventPlugin();
+
 const ContentContainer = React.createClass({
+
+    mixins: [
+        StyleResizable,
+    ],
+
+    getInitialState() {
+        return {
+            muiTheme: getMuiTheme()
+        };
+    },
+
+    componentWillMount() {
+        this.setState({
+            muiTheme: this.state.muiTheme,
+        });
+    },
+
+
     render() {
         console.log("ContentContainer.render()", this.props.children);
 
@@ -50,53 +84,6 @@ const ContentContainer = React.createClass({
 });
 
 
-// define and export our Layout component
-export const Layout = ({content}) => (
-    <div id="app">
-        {console.log("Layout rendered")}
-        <Provider store={store}>
-            <div>
-                <div className="wrapper">
-                    <div className="box">
-                        <div className="row row-offcanvas row-offcanvas-left">
-
-                            <Sidebar/>
-
-                            <div className="column col-sm-10 col-xs-11" id="main">
-
-                                <TopNav/>
-
-                                <div className="padding">
-                                    <div className="full col-sm-9">
-
-                                        <div className="row">
-
-                                            <main>
-                                                <div id="popup"></div>
-
-                                                <div id="outerContent">
-
-                                                    <ContentContainer key="content">
-                                                        {content}
-                                                    </ContentContainer>
-
-                                                    {/*<DevTools />*/}
-
-                                                </div>
-                                            </main>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </Provider>
-    </div >
-
-);
 //<TopNav/>
 //<hr /><div id="popup-target"></div>
 //<div>{content}</div>
@@ -112,5 +99,59 @@ export const Layout = ({content}) => (
 //    })}
 //</ReactCSSTransitionGroup>
 
+let appStyle = {
+   //color:"red"
+};
 
 //export default Layout;
+// define and export our Layout component
+export const Layout = ({content}) => (
+    <div id="app" style={ appStyle }>
+        {console.log("Layout rendered")}
+        <Provider store={store}>
+            <div>
+                <div className="wrapper">
+                    <TopNavContainer store={store}/>
+                    <div className="box">
+                        <div className="row row-offcanvas row-offcanvas-left">
+
+                            <MainContentContainer store={store}>
+                                <div>
+                                    <SidebarContainer store={store}/>
+
+                                    <div className="column col-sm-10 col-xs-11" id="main">
+
+
+                                        <div className="padding">
+                                            <div className="full col-sm-9">
+
+                                                <div className="row">
+
+                                                    <main>
+                                                        <div id="popup"></div>
+
+                                                        <div id="outerContent">
+
+                                                            <ContentContainer key="content">
+                                                                {content}
+                                                            </ContentContainer>
+
+                                                            {/*<DevTools />*/}
+
+                                                        </div>
+                                                    </main>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </MainContentContainer>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Provider>
+    </div >
+
+);
